2994ad08ae082ec4dcae8bc47c49ec2d85b02e72
Nguyen Sang <nguyenvansang30102001@gmail.com>
2022-02-24 09:27:57 +0700

    Fix menu bar change path

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/Login/Login";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { AuthContext } from "./authContext/AuthContext";
 import Main from "./component/Main/Main";
 import Loading from "./component/Loading/Loading";
-import RequestStudent from "./pages/RequestStudent/RequestStudent";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+import useAxios from "./Helper/API";
+import { Logout } from "./authContext/AuthAction";
+import { logout } from "./authContext/CallApi";
 function App() {
-  const { user, isFetching } = useContext(AuthContext);
-  // console.log(user);
+  const { user, isFetching, dispatch } = useContext(AuthContext);
+  const { Client, Loading } = useAxios();
+  const CheckSession = () => {
+    Client.get("/auth/admin/me")
+      .then((response) => {
+        // console.log(response);
+        if (response.data.status !== "Success") {
+          logout(dispatch);
+        }
+      })
+      .catch((err) => {
+        alert("Có lỗi");
+        logout(dispatch);
+      });
+  };
+  useEffect(() => {
+    user && CheckSession();
+  }, []);
+
   return (
     <div className="App">
       {isFetching && <Loading />}
--- src/pages/Notification/Notifications/Notifications.js
+++ src/pages/Notification/Notifications/Notifications.js
@@ -7,105 +7,101 @@ import queryString from "query-string";
 import useAxios from "../../../Helper/API";
 import StoreNotification from "../StoreNotification/StoreNotification";
 
-function Notification(){
+function Notification() {
+  const { Client } = useAxios();
 
-    const { Client } = useAxios();
-  
-    const [Err, setErr] = useState(null);
+  const [Err, setErr] = useState(null);
 
-    const [TieuDe, setTieuDe] = useState([]);
+  const [TieuDe, setTieuDe] = useState([]);
 
-    const [paginations, setPaginations] = useState({
-        limit: 10,
-        page: 1,
-        TotalPage: 1,
-    });
-    const [filter, setfilter] = useState({
-        limit: 10,
-        page: 1,
-    });
-    
-    const [deleteNoti, setDeleteNoti] = useState(false);
+  const [paginations, setPaginations] = useState({
+    limit: 10,
+    page: 1,
+    TotalPage: 1,
+  });
+  const [filter, setfilter] = useState({
+    limit: 10,
+    page: 1,
+  });
 
-    useEffect(() => {
-        const params = queryString.stringify(filter);
-        Client.get("/notification-management/index-header-notification?" + params)
-          .then((response) => {
-            const List = response.data;
-            if (List.status === "Success") {
-              setPaginations(List.pagination);
-              setTieuDe(List.data);
-            }
-          })
-          .catch((err) => {
-            setErr(true);
-          });
-      }, [filter, deleteNoti]);
+  const [deleteNoti, setDeleteNoti] = useState(false);
 
-    const Time = useRef(null);
-    const ChangeLimit = (e) => {
-        const input = e.target;
-        if (Time.current) {
-            clearTimeout(Time.current);
+  useEffect(() => {
+    const params = queryString.stringify(filter);
+    Client.get("/notification-management/index-header-notification?" + params)
+      .then((response) => {
+        const List = response.data;
+        // console.log(List);
+        if (List.status === "Success") {
+          setPaginations(List.pagination);
+          setTieuDe(List.data);
+        } else {
+          setTieuDe([]);
         }
-        Time.current = setTimeout(() => {
-            setfilter({ ...filter, limit: input.value });
-        }, 300);
-    };
+      })
+      .catch((err) => {
+        setErr(true);
+      });
+  }, [filter, deleteNoti]);
 
-    // show hide
-    const [Noti, setNoti] = React.useState(true)
-    const [StoreNoti, setStoreNoti] = React.useState(false)
-
-    const onClickStore = () => 
-    {
-        setStoreNoti(true);
-        setNoti(false);
+  const Time = useRef(null);
+  const ChangeLimit = (e) => {
+    const input = e.target;
+    if (Time.current) {
+      clearTimeout(Time.current);
     }
+    Time.current = setTimeout(() => {
+      setfilter({ ...filter, limit: input.value });
+    }, 300);
+  };
 
-    return (
-        <React.Fragment>
-            <div>
-            { StoreNoti ? <StoreNotification /> : null }
-            { Noti ? (
-                <div className={style.MainNoti}>
-                    <h1 className={style.Hearder_text}>THÔNG BÁO</h1>
-                    <div className={style.Content}>
-                        <div className={style.MainContent}>
-                            {/* Danh sach tieu de thong bao */}
-                            {TieuDe.length != 0 ? (
-                                <ItemNotification 
-                                    data = {TieuDe}
-                                    datadelete = {deleteNoti}
-                                    setDelete = {setDeleteNoti}
-                                />
-                                ) : 
-                                <div className={style.NotiNo}>
-                                    Danh sách thông báo trống!
-                                </div>
-                            }
-                        </div>
-                        <div className={style.PaginationNoti}>
-                            <Pagination
-                                title="Số sinh viên / Trang"
-                                paginations={paginations}
-                                filter={filter}
-                                setfilter={setfilter}
-                                ChangeLimit={ChangeLimit}
-                            />
-                        </div>
-                    </div>
-                    <div className={style.button}>
-                        <Button 
-                            content="Tạo thông báo" 
-                            onClick={onClickStore}
-                        />
-                    </div>
-                </div>
-            ) : null}
+  // show hide
+  const [Noti, setNoti] = React.useState(true);
+  const [StoreNoti, setStoreNoti] = React.useState(false);
+
+  const onClickStore = () => {
+    setStoreNoti(true);
+    setNoti(false);
+  };
+
+  return (
+    <React.Fragment>
+      <div>
+        {StoreNoti ? <StoreNotification /> : null}
+        {Noti ? (
+          <div className={style.MainNoti}>
+            <h1 className={style.Hearder_text}>THÔNG BÁO</h1>
+            <div className={style.Content}>
+              <div className={style.MainContent}>
+                {/* Danh sach tieu de thong bao */}
+                {TieuDe.length != 0 ? (
+                  <ItemNotification
+                    data={TieuDe}
+                    datadelete={deleteNoti}
+                    setDelete={setDeleteNoti}
+                  />
+                ) : (
+                  <div className={style.NotiNo}>Danh sách thông báo trống!</div>
+                )}
+              </div>
+              <div className={style.PaginationNoti}>
+                <Pagination
+                  title="Số sinh viên / Trang"
+                  paginations={paginations}
+                  filter={filter}
+                  setfilter={setfilter}
+                  ChangeLimit={ChangeLimit}
+                />
+              </div>
             </div>
-        </React.Fragment>
-    )
+            <div className={style.button}>
+              <Button content="Tạo thông báo" onClick={onClickStore} />
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Notification;
