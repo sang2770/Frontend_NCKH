0bfbffa6f85ca46df7095a1b309d2378a7ef3b6b
sang2770 <72650632+sang2770@users.noreply.github.com>
2022-05-09 11:26:57 +0700

    Add Data

---
+++ src/Helper/Date.js
@@ -0,0 +1,13 @@
+const FormatDate = (date) => {
+  date = new Date(date);
+  return (
+    date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear()
+  );
+};
+
+const FormatInput = (date) => {
+  const data = date.split("-");
+  console.log(data);
+  return data[2] + "-" + data[1] + "-" + data[0];
+};
+export { FormatDate, FormatInput };
